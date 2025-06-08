1895b0065efbb8459d7ff8d2eba100ee09abfb5d
luojiaaoo <675925864@qq.com>
2025-06-09 00:27:15 +0800

    for test

--- test4.py
+++ test4.py
@@ -1,4 +1,3 @@
 from k3q_arxml import autosar
-from k3q_arxml.arxml_binding.autosar_00052  import autosar_00052
-from k3q_arxml.tools import is_leaf,find_all_ref_xml_obs
-find_all_ref_xml_obs(autosar_00052)
+from k3q_arxml.tools import get_leaf_clazz
+print(get_leaf_clazz(autosar))
